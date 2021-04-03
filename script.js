65cf37723d01815c00f8ed2a661ca48f9a195c0f
Максим Федосов <>
2021-04-03 22:56:24 +0000

    Сделал тестовое объединение двух картинок

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 function getImg(x, y) {
     var img = document.createElement('img')
-    //img.setAttribute('src', 'https://doyoupaint.com/schema?article=A05&x=' + x +'&y=' + y)
-    img.setAttribute('src', 'https://via.placeholder.com/'+(x+1)*10+'x100');
+    var src = 'https://doyoupaint.com/schema?article=A05&x=' + x +'&y=' + y;
+    //var src = 'https://via.placeholder.com/100x100';
+    img.setAttribute('src', src);
     return img
 }
 
 var body = document.getElementsByTagName('body')[0];
 var canvas = document.createElement('canvas');
-canvas.setAttribute('width', 800);
-canvas.setAttribute('height', 800);
+canvas.setAttribute('width', 2800);
+canvas.setAttribute('height', 2800);
 body.appendChild(canvas);
 var ctx = canvas.getContext('2d');
 ctx.fillStyle = "#ff9c38";
 ctx.fillRect(0, 0, 800, 800);
 
-for (var column = 0; column < 2; column++) {
-    for (var row = 0; row < 2; row++) {
-        var img = getImg(x, 0);
-        const elementSize = 100;
-        var x = row * elementSize;
-        var y = column * elementSize;
+var images = [];
+var loadedImagesCount = 0;
+const columnsCount = 2;
+const rowsCount = 1;
+for (var column = 0; column < columnsCount; column++) {
+    for (var row = 0; row < rowsCount; row++) {
+        const elementSize = 630;
+        var x = column * elementSize;
+        var y = row * elementSize;
+        var img = getImg(x, y);
+        images.push(img);
         img.onload = function () {
+            loadedImagesCount++;
+            if (loadedImagesCount == images.length) {
+                allImagesLoaded();
+            }
+        }
+    }
+}
+
+function allImagesLoaded() {
+    console.log('all loaded ' + loadedImagesCount);
+    for (var column = 0; column < columnsCount; column++) {
+        for (var row = 0; row < rowsCount; row++) {
+            const elementSize = 798;
+            var x = column * elementSize;
+            var y = row * elementSize;
+            var i = row * rowsCount + column;
+            var img = images[i];
             ctx.drawImage(img, x, y);
             console.log('draw '+x+' '+y);
         }
