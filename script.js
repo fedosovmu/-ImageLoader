3738318e24be1008af71b4fbbde9883442e737b1
Максим Федосов <>
2021-04-04 18:50:45 +0000

    Отрефакторил код

--- script.js
+++ script.js
@@ -7,39 +7,41 @@ function getImg(x, y) {
     return img
 }
 
+var images = [];
 const columnsCount = 2;
 const rowsCount = 2;
-const oneImageSize = 800;
 
-var body = document.getElementsByTagName('body')[0];
-var canvas = document.createElement('canvas');
-canvas.setAttribute('width', columnsCount * oneImageSize);
-canvas.setAttribute('height', rowsCount * oneImageSize);
-body.appendChild(canvas);
-var ctx = canvas.getContext('2d');
-ctx.fillStyle = "#ff9c38";
-ctx.fillRect(0, 0, 800, 400);
-
-var images = [];
-var loadedImagesCount = 0;
-for (var row = 0; row < rowsCount; row++) {
-    for (var column = 0; column < columnsCount; column++) {
-        const elementSize = 630;
-        var x = column * elementSize;
-        var y = row * elementSize;
-        var img = getImg(x, y);
-        images.push(img);
-        img.onload = function () {
-            loadedImagesCount++;
-            if (loadedImagesCount == images.length) {
-                allImagesLoaded();
+function loadImages (onLoad) {
+    var loadedImagesCount = 0;
+    for (var row = 0; row < rowsCount; row++) {
+        for (var column = 0; column < columnsCount; column++) {
+            const elementSize = 630;
+            var x = column * elementSize;
+            var y = row * elementSize;
+            var img = getImg(x, y);
+            images.push(img);
+            img.onload = function () {
+                loadedImagesCount++;
+                if (loadedImagesCount == images.length) {
+                    console.log('all loaded ' + loadedImagesCount);
+                    onLoad();
+                }
             }
         }
     }
 }
 
-function allImagesLoaded() {
-    console.log('all loaded ' + loadedImagesCount);
+function drawImages() {
+    const oneImageSize = 800;
+    var body = document.getElementsByTagName('body')[0];
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('width', columnsCount * oneImageSize);
+    canvas.setAttribute('height', rowsCount * oneImageSize);
+    body.appendChild(canvas);
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "#ff9c38";
+    ctx.fillRect(0, 0, 800, 400);
+
     for (var row = 0; row < rowsCount; row++) {
         for (var column = 0; column < columnsCount; column++) {
             const elementSize = 798;
@@ -53,6 +55,4 @@ function allImagesLoaded() {
     }
 }
 
-//ctx.fillRect(10, 10, 150, 100);
-//ctx.drawImage(img, 0, 0)
-//ctx.fillRect(10, 10, 150, 100);
+loadImages(drawImages);
